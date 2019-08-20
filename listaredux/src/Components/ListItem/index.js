fb1cc20fc030b37f1e8f3bbfbcd2f9578fd6bd0c
Angel Barusta <angel.barusta.95@gmail.com>
2019-08-19 21:57:13 -0500

    items

--- listaredux/src/Components/ListItem/index.js
+++ listaredux/src/Components/ListItem/index.js
@@ -1,12 +1,28 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import { Button, Popup, Item, Image } from 'semantic-ui-react'
 import '../../App.css';
+import {ItemTask} from './item.json';
+
 
 class ListaItems extends Component{
     render(){
+        const ItemGrup=ItemTask.map((s,i)=>{
+            return(
+                <Item.Group style={{padding:20 }}>
+                 <Item>
+                  <Item.Content style={{background: 'chocolate',padding:10}}>
+                  <h4 style={{margin:'auto'}}>elemento # {i+1}</h4>
+                   <p> {s.nombre}</p>                    
+                  </Item.Content>
+                </Item>
+              </Item.Group>
+            )
+        });
+
         return(
             <div className='barListItem'>
-                ListaItems
+               {ItemGrup}
             </div>
         )
     }
